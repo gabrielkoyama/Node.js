5a9049f1ba7d5f885ca8a35314bdb39b124b3ad2
Gabriel Koyama <gabriel.k.alves@hotmail.com>
2017-11-29 00:19:50 -0200

    Finish

--- routes/cadastro.js
+++ routes/cadastro.js
@@ -15,50 +15,44 @@ router.get('/cadastro/edit', function(req, res, next) {
   res.render('edit', { title: 'Edite'});
   });
 
-// 'METODOS'
+// CRUD:
 
-// ADD
-router.post('/add', function(req, res, next) {
-  var objeto = {nome:req.body.nome,idade:req.body.idade};
-  MongoClient.connect(config.mongoUrl, function( err, db ){    
-    db.collection('fulanos').insert(objeto);
-  })
- 
-  res.redirect('/');
-
-});
-
-// EDITAR
-router.get('/edit/:id', function(req, res, next) {
-  var id = ObjectID(req.params.id)
-  MongoClient.connect(config.mongoUrl, function( err, db ){
-    db.collection('fulanos').find({_id:id}).toArray(function( err, data){
-      res.render('edit', {title:'edite seu cadastro', petecas:data});      
-    })
-  })
-});
-
-// UPDATE
-router.post('/update', function(req, res, next) {
-  
+  // CREATE
+  router.post('/add', function(req, res, next) {
     var objeto = {nome:req.body.nome,idade:req.body.idade};
-    var id = ObjectID(req.body.id)
-
-    MongoClient.connect(config.mongoUrl, function( err, db ){
-      db.collection('fulanos').update({_id:id},(objeto));
+    MongoClient.connect(config.mongoUrl, function( err, db ){    
+      db.collection('fulanos').insert(objeto);
     })
     res.redirect('/');
   });
 
-  // DELETE
-  router.get('/delete/:id', function(req, res, next) {
-    
-      var objeto = {nome:req.body.nome,idade:req.body.idade};
-      var id = ObjectID(req.params.id)
-  
-      MongoClient.connect(config.mongoUrl, function( err, db ){
-        db.collection('fulanos').remove({_id:id});
+  // READ
+  router.get('/edit/:id', function(req, res, next) {
+    var id = ObjectID(req.params.id)
+    MongoClient.connect(config.mongoUrl, function( err, db ){
+      db.collection('fulanos').find({_id:id}).toArray(function( err, data){
+        res.render('edit', {title:'edite seu cadastro', petecas:data});      
       })
+    })
+  });
+
+  // UPDATE
+  router.post('/update', function(req, res, next) {
+      var objeto = {nome:req.body.nome,idade:req.body.idade};
+      var id = ObjectID(req.body.id)
+      MongoClient.connect(config.mongoUrl, function( err, db ){
+        db.collection('fulanos').update({_id:id},(objeto));
+      })
+    res.redirect('/');
+  });
+
+    // DELETE
+    router.get('/delete/:id', function(req, res, next) {
+        var objeto = {nome:req.body.nome,idade:req.body.idade};
+        var id = ObjectID(req.params.id)
+        MongoClient.connect(config.mongoUrl, function( err, db ){
+          db.collection('fulanos').remove({_id:id});
+        })
       res.redirect('/');
     });
 
